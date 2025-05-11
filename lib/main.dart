d5de1ef591b20ea9f058b8a38e6e9947266610a2
khanshairer <76740529+khanshairer@users.noreply.github.com>
2025-05-11 14:58:43 -0230

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Zoo App',
+      title: 'Miscellaneous App by Sharier Khan',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
